8c5d25b390dbac6e9a2feaf5c97a0b090a973759
Edson Neto <edsonnetowr@gmail.com>
2025-10-06 17:54:15 +0100

    feat: :sparkles: structural adjustment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <main class="relative flex flex-col flex-1 container mx-auto py-8">
+  <main class="relative flex flex-col flex-1 container mx-auto px-8 py-6">
+    <hr class="w-1/5 rotate-90 absolute right-2 rounded-2xl top-2" />
+    <Navbar />
     <slot />
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,126 +1,20 @@
 <template>
-  <div class="flex flex-row flex-1 gap-6">
-    <div class="flex flex-col gap-4 basis-1/3 h-full">
-      <div
-        class="flex flex-col flex-1 items-center gap-4 py-8 px-6 border-4 border-solid border-gray-800"
-      >
-        <!-- My Picture goes here -->
-        <div
-          class="bg-gray-200 h-40 w-40 rounded-full border-2 border-solid border-gray-800"
-        ></div>
-
-        <div class="flex flex-col gap-2 justify-center items-center">
-          <h1>Edson Neto</h1>
-          <h3>Senior Full Stack developer</h3>
-        </div>
-
-        <div class="flex flex-col gap-4">
-          <h2>Profile</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            viverra finibus quam at facilisis. Nunc sit amet viverra ipsum.
-            Nulla nec velit vel neque sodales dictum et at augue. Sed vitae
-            lacinia leo. Morbi erat ex, sagittis a molestie non, lacinia vel
-            felis. Proin porttitor consectetur condimentum. Proin pellentesque
-            massa commodo ante rutrum, quis consequat lorem rhoncus.
-          </p>
-        </div>
-        <div class="flex flex-col gap-4">
-          <h2>Hobbies</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            viverra finibus quam at facilisis. Nunc sit amet viverra ipsum.
-            Nulla nec velit vel neque sodales dictum et at augue. Sed vitae
-            lacinia leo. Morbi erat ex, sagittis a molestie non, lacinia vel
-            felis. Proin porttitor consectetur condimentum. Proin pellentesque
-            massa commodo ante rutrum, quis consequat lorem rhoncus.
-          </p>
-        </div>
-      </div>
-      <div class="flex flex-col bg-slate-900">
-        <div class="flex flex-col gap-4 py-4 px-8">
-          <h2 class="text-white">Social Media</h2>
-          <span class="flex gap-4">
-            <span class="h-8 w-8 bg-white rounded-full"></span>
-            <span class="text-white">Github</span>
-          </span>
-          <span class="flex gap-4">
-            <span class="h-8 w-8 bg-white rounded-full"></span>
-            <span class="text-white">LinkedIn</span>
-          </span>
-        </div>
+  <div class="relative flex my-auto justify-between w-full">
+    <div class="flex flex-col gap-2 text-right">
+      <h3 class="text-4xl">Edson Neto's</h3>
+      <h1 class="text-8xl">Portfolio</h1>
+    </div>
+    <div class="flex flex-col gap-2">
+      <NuxtPicture format="avif,webp" src="/me.png" />
+      <div class="inline-flex gap-1 py-0.5 border-b w-fit mx-auto">
+        <Icon name="fa7-brands:github" width="16" height="16" />
+        <Icon name="fa7-brands:linkedin" width="16" height="16" />
+        <Icon name="fa7-solid:envelope" width="16" height="16" />
       </div>
     </div>
-    <div class="flex flex-col gap-6 basis-2/3">
-      <div class="flex flex-col gap-4">
-        <span class="pb-1 border-b-2 border-solid border-b-gray-900">
-          <h1>Skills</h1>
-        </span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          viverra finibus quam at facilisis. Nunc sit amet viverra ipsum. Nulla
-          nec velit vel neque sodales dictum et at augue. Sed vitae lacinia leo.
-          Morbi erat ex, sagittis a molestie non, lacinia vel felis. Proin
-          porttitor consectetur condimentum. Proin pellentesque massa commodo
-          ante rutrum, quis consequat lorem rhoncus.
-        </p>
-        <div class="flex gap-8 max-w-2xl mx-auto flex-wrap">
-          <DonutChart
-            icon="https://api.iconify.design/cib:vue-js.svg"
-            :percentage="100"
-            text="Vuejs"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:react.svg"
-            :percentage="70"
-            text="React"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:node-js.svg"
-            :percentage="100"
-            text="NodeJs"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:javascript.svg"
-            :percentage="70"
-            text="JavaScript"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:mongodb.svg"
-            :percentage="50"
-            text="MongoDB"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:postgresql.svg"
-            :percentage="40"
-            text="PostgreSQL"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:angular.svg"
-            :percentage="80"
-            text="Angular"
-          />
-          <DonutChart
-            icon="https://api.iconify.design/cib:flutter.svg"
-            :percentage="70"
-            text="Flutter"
-          />
-        </div>
-      </div>
-
-      <div class="flex flex-col gap-4">
-        <span class="pb-1 border-b-2 border-solid border-b-gray-900">
-          <h1>Education</h1>
-        </span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          viverra finibus quam at facilisis. Nunc sit amet viverra ipsum. Nulla
-          nec velit vel neque sodales dictum et at augue. Sed vitae lacinia leo.
-          Morbi erat ex, sagittis a molestie non, lacinia vel felis. Proin
-          porttitor consectetur condimentum. Proin pellentesque massa commodo
-          ante rutrum, quis consequat lorem rhoncus.
-        </p>
-      </div>
+    <div class="absolute top-full left-0 flex flex-col gap-2 w-14 h-fit flex-nowrap">
+      <h4 class="whitespace-pre-line">Senior Fullstack</h4>
+      <span class="flex border-l h-24 w-1"></span>
     </div>
   </div>
 </template>
@@ -129,7 +23,7 @@
 export default defineNuxtComponent({
   head() {
     return {
-      title: "Edson Neto",
+      title: "Hi, I'm Edson Neto",
     };
   },
   setup() {
